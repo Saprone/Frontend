0e16024fdfa98f97dc663e209f6977e43c24b433
Bas <onrust.bas@gmail.com>
2024-12-09 16:08:22 +0100

    post request works

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
-  <UserComponent />
-  <IngredientComponent />
+  <SimpleForm />
 </template>
 
 <script>
-import UserComponent from './components/UserComponent.vue';
-import IngredientComponent from './components/IngredientComponent.vue';
+//import UserComponent from './components/UserComponent.vue';
+//import IngredientComponent from './components/IngredientComponent.vue';
+import SimpleForm from './components/SimpleForm.vue';
 
 export default {
   name: 'App',
   components: {
-    UserComponent,
-    IngredientComponent,
+    /*UserComponent,
+    IngredientComponent,*/
+    SimpleForm 
   }
 }
 </script>
---
+++ src/components/SimpleForm.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>Post request</h1>
+    <textarea v-model="data" placeholder="Enter some data..."></textarea>
+    <button @click="sendData">Send Data</button>
+    <p v-if="response">{{ response }}</p>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      data: '',
+      response: '',
+    };
+  },
+  methods: {
+    async sendData() {
+      //get request
+      //const response = await axios.get('http://localhost:8222/basket/get');
+      //console.log("get: "+response.data);
+
+      //post request
+      const response = await axios.post('http://localhost:8222/basket/post', this.data);
+      console.log("post: "+response.data);
+    },
+  },
+};
+</script>
